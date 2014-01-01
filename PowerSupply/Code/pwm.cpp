2733aaa6438803cfb2b75daba7d444f998f1bb75
unknown <TS@TS-PC.(none)>
2014-01-02 01:04:05 +0900

    GPL licences added

--- PowerSupply/Code/pwm.cpp
+++ PowerSupply/Code/pwm.cpp
@@ -1,12 +1,24 @@
-/* Copyright (C) 2012 Renesas Electronics Corporation. All rights reserved.   */
-/*******************************************************************************
-* File Name    : pwm.c
-* Version     : 1.00
-* Device     : R5F5630E
-* Tool-Chain   : Renesas RX Toolchain 1.2.0.0
-* H/W Platform  : RSKRX630
-* Description   : Defines MTU3 configuration functions
-*******************************************************************************/
+/*
+ *  Copyright (c) 2013 Tonu Samuel
+ *  All rights reserved.
+ *
+ *  This file is part of robot "Kuukulgur".
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 /* Following header file provides standard integer type definitions. */
 #include <stdint.h>
 #include <stdio.h>
--- PowerSupply/Code/switch.h
+++ PowerSupply/Code/switch.h
@@ -1,26 +1,23 @@
-/* Adapted for use with IAR Embedded Workbench */
-/*******************************************************************************
-* DISCLAIMER
-* This software is supplied by Renesas Electronics Corporation and is only
-* intended for use with Renesas products. No other uses are authorized. This
-* software is owned by Renesas Electronics Corporation and is protected under
-* all applicable laws, including copyright laws.
-* THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING
-* THIS SOFTWARE, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT
-* LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
-* AND NON-INFRINGEMENT. ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.
-* TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY LAW, NEITHER RENESAS
-* ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE
-* FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR
-* ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR ITS AFFILIATES HAVE
-* BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-* Renesas reserves the right, without notice, to make changes to this software
-* and to discontinue the availability of this software. By using this software,
-* you agree to the additional terms and conditions found by accessing the
-* following link:
-* http://www.renesas.com/disclaimer
-*******************************************************************************/
-/* Copyright (C) 2012 Renesas Electronics Corporation. All rights reserved.   */
+/*
+ *  Copyright (c) 2013 Tonu Samuel
+ *  All rights reserved.
+ *
+ *  This file is part of robot "Kuukulgur".
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 /*******************************************************************************
 * File Name    : switch.h
 * Version     : 1.00
